55b416da87341f6425911ee6c79c5ab7796ae550
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-04-28 16:22:57 +0300

    Update threadpool.h

--- threadpool.h
+++ threadpool.h
@@ -1,6 +1,21 @@
 
 #define LOCK_MUTEX(m)   pthread_mutex_lock(m)
 #define UNLOCK_MUTEX(m) pthread_mutex_unlock(m)
+// Structure to represent a thread
+typedef struct Thread {
+    int id;                        
+    pthread_t pthread;              
+    struct ThreadPool* threadPoolPtr;    
+    int status;                    
+
+} *Pthread;
+
+// Structure to represent a thread pool
+typedef struct ThreadPool {
+    Pthread* threads;                      
+    TaskQueue taskQueue;                   
+    int maxThreadCount;                     
+} ThreadPool, *PThreadPool;
 typedef struct threadpool ThreadPool;
 
 ThreadPool *threadpool_create(int num_threads);
