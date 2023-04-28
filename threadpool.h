1b40b0b17f4badef9f3b85db8dbca99996eaaebb
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-04-28 16:21:55 +0300

    Update threadpool.h

--- threadpool.h
+++ threadpool.h
@@ -1,6 +1,6 @@
-#ifndef THREADPOOL_H
-#define THREADPOOL_H
 
+#define LOCK_MUTEX(m)   pthread_mutex_lock(m)
+#define UNLOCK_MUTEX(m) pthread_mutex_unlock(m)
 typedef struct threadpool ThreadPool;
 
 ThreadPool *threadpool_create(int num_threads);
@@ -9,4 +9,4 @@ void threadpool_destroy(ThreadPool *pool);
 
 void threadpool_submit(ThreadPool *pool, void (*function)(void *), void *data);
 
-#endif /* THREADPOOL_H */
+
