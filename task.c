26772a7f410a67cdd2479a72365ed6fb993d7334
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-04-29 12:03:35 +0300

    Add files via upload

--- task.c
+++ task.c
@@ -1,24 +1,24 @@
 #include "task.h"
 
 // Helper function to initialize semaphore and return an error code if allocation fails
-int initialize_semaphore(PTaskQueue p_task) {
-    p_task->has_tasks = (Psemaphore)calloc(1, sizeof(semaphore));
-    if (p_task->has_tasks == NULL) {
+int initialize_semaphore(PTaskQueue PTask) {
+    PTask->has_tasks = (Psemaphore)calloc(1, sizeof(semaphore));
+    if (PTask->has_tasks == NULL) {
         return -1;
     }
-    semaphore_init(p_task->has_tasks, 0);
+    semaphore_init(PTask->has_tasks, 0);
     return 0;
 }
 
 // Helper function to clean up allocated memory when mutex initialization fails
-void cleanup_memory(PTaskQueue p_task) {
-    free(p_task->has_tasks);
-    p_task->has_tasks = NULL;
+void cleanup_memory(PTaskQueue PTask) {
+    free(PTask->has_tasks);
+    PTask->has_tasks = NULL;
 }
 
 // Helper function to acquire lock on mutex and return an error code if lock fails
-int acquire_lock(PTaskQueue p_task) {
-    int mutex_lock_result = pthread_mutex_lock(&p_task->mutex);
+int acquire_lock(PTaskQueue PTask) {
+    int mutex_lock_result = pthread_mutex_lock(&PTask->mutex);
     if (mutex_lock_result != 0) {
         return -1;
     }
@@ -26,115 +26,148 @@ int acquire_lock(PTaskQueue p_task) {
 }
 
 // Helper function to release lock on mutex
-void release_lock(PTaskQueue p_task) {
-    pthread_mutex_unlock(&p_task->mutex);
+void release_lock(PTaskQueue PTask) {
+    pthread_mutex_unlock(&PTask->mutex);
 }
 
 // Function to initialize task queue with size 0 and empty head and tail
-int TaskQueue_init(PTaskQueue p_task) {
-    if (p_task == NULL) {
+int TaskQueue_init(PTaskQueue PTask) {
+    if (PTask == NULL) {
         return -1;
     }
-    p_task->size = 0;
-    p_task->head = NULL;
-    p_task->tail = NULL;
-    if (initialize_semaphore(p_task) != 0) {
+    PTask->size = 0;
+    PTask->head = NULL;
+    PTask->tail = NULL;
+    if (initialize_semaphore(PTask) != 0) {
         return -1;
     }
-    if (pthread_mutex_init(&(p_task->mutex), NULL) != 0) {
-        cleanup_memory(p_task);
+    if (pthread_mutex_init(&(PTask->mutex), NULL) != 0) {
+        cleanup_memory(PTask);
         return -1;
     }
     return 0;
 }
 
 // Function to add a new task to the end of the task queue
-void TaskQueue_push(PTaskQueue p_task, Ptask Pnew_task) {
-    if (p_task == NULL || Pnew_task == NULL) {
+void TaskQueue_push(PTaskQueue PTask, Ptask PTask_new) {
+    if (PTask == NULL || PTask_new == NULL) {
         return;
     }
-    if (acquire_lock(p_task) != 0) {
+    if (acquire_lock(PTask) != 0) {
         return;
     }
-    Pnew_task->previous_task = NULL;
-    if (p_task->size == 0) {
-        p_task->head = Pnew_task;
+    PTask_new->previous_task = NULL;
+    if (PTask->size == 0) {
+        PTask->head = PTask_new;
     } else {
-        p_task->tail->previous_task = Pnew_task;
+        PTask->tail->previous_task = PTask_new;
     }
-    p_task->tail = Pnew_task;
-    p_task->size++;
-    release_semaphore(p_task->has_tasks);
-    release_lock(p_task);
+    PTask->tail = PTask_new;
+    PTask->size++;
+    release_semaphore(PTask->has_tasks);
+    release_lock(PTask);
 }
 
 
-Ptask TaskQueue_pull(PTaskQueue p_task)
-{
-    // Check if the input pointer is NULL
-    if (p_task == NULL) {
+// Acquires the lock on the mutex and returns an error code if it fails
+int acquire_mutex_lock(PTaskQueue PTask) {
+    return pthread_mutex_lock(&PTask->mutex);
+}
+
+// Releases the lock on the mutex
+void release_mutex_lock(PTaskQueue PTask) {
+    pthread_mutex_unlock(&PTask->mutex);
+}
+
+// Checks if the task queue is empty
+int is_queue_empty(PTaskQueue PTask) {
+    return PTask->size == 0;
+}
+
+// Updates the head, tail, and size of the task queue when it has only one task
+void update_queue_single_task(PTaskQueue PTask) {
+    PTask->head = NULL;
+    PTask->tail = NULL;
+    PTask->size = 0;
+}
+
+// Updates the head and size of the task queue when it has more than one task
+void update_queue_multiple_tasks(PTaskQueue PTask, Ptask curr_task) {
+    PTask->head = curr_task->previous_task;
+    PTask->size--;
+    release_semaphore(PTask->has_tasks);
+}
+
+// Pulls a task from the head of the task queue
+Ptask pull_task(PTaskQueue PTask) {
+    Ptask curr_task = PTask->head;
+
+    if (is_queue_empty(PTask)) {
         return NULL;
     }
-    // Acquire lock on the mutex
-    int mutex_lock_result = pthread_mutex_lock(&p_task->mutex);
 
-    // Check if the mutex lock failed
-    if (mutex_lock_result != 0) {
-        return NULL;
-    }
-    // Get the current task from the head of the queue
-    Ptask curr_task = p_task->head;
-
-    // Check if there are no tasks in the queue
-    if (p_task->size == 0) {
-        // Release the lock and return NULL
-        pthread_mutex_unlock(&p_task->mutex);
-        return NULL;
-    }
-
-    // Check if there is only one task in the queue
-    if (p_task->size == 1) {
-        // Update the head, tail, and size of the queue
-        p_task->head = NULL;
-        p_task->tail = NULL;
-        p_task->size = 0;
+    if (PTask->size == 1) {
+        update_queue_single_task(PTask);
     } else {
-        // If there are more than one tasks in the queue
-        // Update the head and size of the queue
-        p_task->head = curr_task->previous_task;
-        p_task->size--;
-        // Signal that there are more tasks in the queue
-        release_semaphore(p_task->has_tasks);
+        update_queue_multiple_tasks(PTask, curr_task);
     }
 
-    // Release the lock
-    pthread_mutex_unlock(&p_task->mutex);
-
-    // Return the current task
     return curr_task;
 }
 
-void TaskQueue_destroy(PTaskQueue p_task)
-{
-
-    // Free all the tasks in the queue
-    while (p_task->size) {
-        Ptask task = TaskQueue_pull(p_task);
-        if (task == NULL) {
-            // Handle the error and continue freeing the remaining tasks in the queue
-            fprintf(stderr, "Error: Failed to pull task from queue.\n");
-            continue;
-        }
-        free(task);
+// Function to pull a task from the task queue
+Ptask TaskQueue_pull(PTaskQueue PTask) {
+    if (PTask == NULL) {
+        return NULL;
     }
 
-    // Reset the head, tail, size, and semaphore
-    p_task->head = NULL;
-    p_task->tail = NULL;
+    int mutex_lock_result = acquire_mutex_lock(PTask);
+    if (mutex_lock_result != 0) {
+        return NULL;
+    }
 
-    semaphore_reset(p_task->has_tasks);
-    p_task->size = 0;
+    Ptask curr_task = pull_task(PTask);
 
-    // Free the semaphore memory
-    free(p_task->has_tasks);
+    release_mutex_lock(PTask);
+
+    return curr_task;
 }
+
+// Frees a single task from the queue
+void free_task(PTaskQueue PTask) {
+    Ptask task = TaskQueue_pull(PTask);
+    if (task == NULL) {
+        // Handle the error and continue freeing the remaining tasks in the queue
+        fprintf(stderr, "Error: Failed to pull task from queue.\n");
+        return;
+    }
+    free(task);
+}
+
+// Frees all tasks in the queue
+void free_all_tasks(PTaskQueue PTask) {
+    while (PTask->size) {
+        free_task(PTask);
+    }
+}
+
+// Resets the head, tail, size, and semaphore
+void reset_queue_state(PTaskQueue PTask) {
+    PTask->head = NULL;
+    PTask->tail = NULL;
+    semaphore_reset(PTask->has_tasks);
+    PTask->size = 0;
+}
+
+// Frees the semaphore memory
+void free_semaphore(PTaskQueue PTask) {
+    free(PTask->has_tasks);
+}
+
+// Destroys the task queue
+void TaskQueue_destroy(PTaskQueue PTask) {
+    free_all_tasks(PTask);
+    reset_queue_state(PTask);
+    free_semaphore(PTask);
+}
+
--- task.h
+++ task.h
@@ -2,6 +2,7 @@
 
 /*struct task: A structure that represents a task to be executed by the TaskQueue. It contains a function pointer to the task function and a pointer to the arguments of the task function.*/
 typedef struct task {
+    int task_id; // Unique identifier for the task
     struct task* previous_task;     // Pointer to the previous task in the queue
     void (*func)(void* arg);    // Function pointer to the task function
     void* arg;    // Pointer to the arguments of the task function
@@ -17,15 +18,53 @@ typedef struct TaskQueue {
     
 } TaskQueue, *PTaskQueue;
 
+// Helper function to initialize semaphore and return an error code if allocation fails
+int initialize_semaphore(PTaskQueue PTask) ;
+
+// Helper function to clean up allocated memory when mutex initialization fails
+void cleanup_memory(PTaskQueue PTask);
+// Helper function to release lock on mutex
+void release_lock(PTaskQueue PTask);
+
+// Helper function to acquire lock on mutex and return an error code if lock fails
+int acquire_lock(PTaskQueue PTask);
+
 /*Initializes a TaskQueue. It takes a pointer to a TaskQueue as an argument and returns an integer indicating success or failure.*/
-int   TaskQueue_init(PTaskQueue P_task);
+int   TaskQueue_init(PTaskQueue PTask);
 
 /* Adds a task to the end of the queue. It takes a pointer to a TaskQueue and a pointer to a task as arguments.*/
-void  TaskQueue_push(PTaskQueue P_task, Ptask Pnew_task);
+void  TaskQueue_push(PTaskQueue PTask, Ptask Pnew_task);
 
 /*Removes and returns the first task from the queue. It takes a pointer to a TaskQueue as an argument and returns a pointer to the removed task.*/
-Ptask TaskQueue_pull(PTaskQueue P_task);
+Ptask TaskQueue_pull(PTaskQueue PTask);
 
+// Pulls a task from the head of the task queue
+Ptask pull_task(PTaskQueue PTask);
+
+// Updates the head and size of the task queue when it has more than one task
+void update_queue_multiple_tasks(PTaskQueue PTask, Ptask curr_task);
+
+// Releases the lock on the mutex
+void release_mutex_lock(PTaskQueue PTask);
+
+// Acquires the lock on the mutex and returns an error code if it fails
+int acquire_mutex_lock(PTaskQueue PTask);
+
+// Checks if the task queue is empty
+int is_queue_empty(PTaskQueue PTask);
+// Updates the head, tail, and size of the task queue when it has only one task
+void update_queue_single_task(PTaskQueue PTask);
 /*Destroys a TaskQueue and frees all memory associated with it. It takes a pointer to a TaskQueue as an argument.*/
-void  TaskQueue_destroy(PTaskQueue P_task);
+void  TaskQueue_destroy(PTaskQueue PTask);
 
+// Frees the semaphore memory
+void free_semaphore(PTaskQueue PTask);
+
+// Frees a single task from the queue
+void free_task(PTaskQueue PTask);
+
+// Frees all tasks in the queue
+void free_all_tasks(PTaskQueue PTask);
+
+// Resets the head, tail, size, and semaphore
+void reset_queue_state(PTaskQueue PTask);
