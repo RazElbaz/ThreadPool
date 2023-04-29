950241ca2fedd17400faba6ebf60f4e12333fc2d
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2023-04-29 11:30:26 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -66,21 +66,29 @@ void new_task(void *arg) {
 
 // Helper function to parse the encryption key and mode from command-line arguments
 void parse_arguments(int argc, char *argv[]) {
+    // Parse the encryption key from the command-line argument
     programData.key = atoi(argv[1]);
+
+    // Parse the encryption/decryption mode from the command-line argument
     programData.mode = argv[2][1];
+
+    // Initialize the current and data indices
     programData.current_index = 0;
     programData.data_index = 0;
-    // Check if the program was run with the correct arguments
+
+    // Check if the program was run with the correct number of arguments
     if (argc < 3) {
         fprintf(stderr, "Usage: %s <key> <-e/-d>\n", argv[0]);
         exit(1);
     }
+
     // Check if the encryption/decryption mode is valid
     if (programData.mode != 'e' && programData.mode != 'd') {
         fprintf(stderr, "Invalid mode: %c, must be -e or -d\n", programData.mode);
         exit(1);
     }
 }
+
 
 // Helper function to initialize the thread pool
 threadpool initialize_threadpool() {
@@ -90,38 +98,53 @@ threadpool initialize_threadpool() {
 
 // Helper function to allocate and read data from stdin
 Pinput allocate_and_read_data() {
+    // Allocate memory for the data item
     Pinput data = (Pinput)calloc(1, sizeof(data));
     if (!data) {
         perror("calloc");
         exit(1);
     }
+
+    // Set the index of the data item
     data->index = programData.current_index;
+
+    // Allocate memory for the data value
     data->value = (char *)malloc(MAX_DATA_COUNT);
     if (!data->value) {
         perror("malloc");
         free(data);
         exit(1);
     }
+
+    // Read data from stdin into the data value
     if (!fgets(data->value, MAX_DATA_COUNT, stdin)) {
+        // End of input, free the allocated memory and return NULL
         free(data->value);
         free(data);
         return NULL;
     }
+
+    // Return the data item
     return data;
 }
 
 // The main function
 int main(int argc, char *argv[]) {
+    // Parse the encryption key and mode from command-line arguments
     parse_arguments(argc, argv);
 
+    // Initialize the thread pool
     threadpool thpool = initialize_threadpool();
 
     // Read data from stdin and add tasks to the thread pool
     while (1) {
+        // Allocate and read data from stdin
         Pinput data = allocate_and_read_data();
         if (data == NULL) {
             break; // End of input, break out of the loop
         }
+
+        // Add a new task to the thread pool to process the data item
         threadpool_new_task(thpool, new_task, data);
         programData.current_index++;
     }
@@ -132,3 +155,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
